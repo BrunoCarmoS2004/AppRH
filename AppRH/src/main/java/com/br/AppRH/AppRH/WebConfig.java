7bdd02e6d1a79588f7fdc2cb111af7dc3ae7a620
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-11-10 17:13:03 -0300

    a

--- AppRH/src/main/java/com/br/AppRH/AppRH/WebConfig.java
+++ AppRH/src/main/java/com/br/AppRH/AppRH/WebConfig.java
@@ -1,7 +1,6 @@
-package com.br.AppRH.AppRH;
+/*package com.br.AppRH.AppRH;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,13 +23,9 @@ public class WebConfig extends WebSecurityConfiguration{
         .and()
         .withUser("root").password("{noop}root").roles("ADMIN");
     }
-   protected SecurityFilterChain  configure(HttpSecurity http) throws Exception{
-    http.security()
-                .antMatchers("/topicos").permitAll()
-                .antMatchers("/topicos/*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
-    return http.build();
-   }
+    
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(null)
+    }
 }
+*/
